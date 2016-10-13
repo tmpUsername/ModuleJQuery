b5946db7405091703e5e668b3b89eb6dfa21ed26
tmpUsername <admin@fddf-PC>
2016-10-13 16:51:18 +0200

    showHide.js: ajout de la fonction

--- public_html/JS/showHide.js
+++ public_html/JS/showHide.js
@@ -3,5 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+var change = true;
 
-
+$(document).ready(function(){
+    $('input').on("click", function(){
+        if (change){
+            $("#change").hide();
+        } else {
+            $("#change").show();
+        }
+        change = !change;
+    });
+});
